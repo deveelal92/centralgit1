4b65bd000fcf627d1956c9491b17e2f56067b45f
Devee Lal <deveelal92@gmail.com>
2026-06-26 16:58:17 +0530

    terraform rg

---
+++ terraform/main.tf
@@ -0,0 +1,4 @@
+resource "azurerm_resource_group" "rg" {
+  name = "github-rg1"
+  location = "central india"
+  }
---
+++ terraform/provider.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    azurerm = {
+        source = "hashicorp/azurerm"
+        version = "4.76.0"
+    }
+  }
+}
+
+provider "azurerm" {
+    features {
+      
+    }
+    subscription_id = "934f0ced-05a0-41ba-8687-01cd039c4deb"
+  
+}
