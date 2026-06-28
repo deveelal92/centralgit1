dec8db5cf0a0be414c32656192e14d1627772f9a
Devee Lal <deveelal92@gmail.com>
2026-06-28 19:54:21 +0530

    Remove broken nested repository

--- .github/workflows/gitrg.tf
+++
@@ -1,4 +0,0 @@
-resource "azurerm_resource_group" "main" {
-name = "github-rg"
-location = "eastus"
-}
--- terraform/main.tf
+++
@@ -1,4 +0,0 @@
-resource "azurerm_resource_group" "rg123" {
-  name = "github-rg21"
-  location = "central india"
-  }
