a7749bf1fdf3ed07860b921cd0b2eea360a103c3
Devee Lal <deveelal92@gmail.com>
2026-06-29 20:28:17 +0530

    terraform provider issue fix

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,8 +1,9 @@
 terraform {
+    required_version = ">= 1.6.0"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "4.76.0"
+      version = "~> 4.0"
     }
   }
 }
